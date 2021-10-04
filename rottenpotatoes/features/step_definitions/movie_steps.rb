34db5391ea88992ae6fbd29fbdbbc17b15ce5f82
evelyn.crowe <evelyn.a.crowe@gmail.com>
2021-10-04 23:29:12 +0000

    part 2. nearly done, just have an issue with unable to find rating checkboxes

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -27,10 +27,20 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  if uncheck 
+    rating_list.split(', ').each do |rating|
+      When I uncheck "ratings_{#rating}"
+    end 
+  else
+    rating_list.split(', ').each do |rating|
+      When I check "ratings_{#rating}"
+    end 
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  Movie.all.each do |movie|
+    I should see "{movie.title}"
+  end
 end
